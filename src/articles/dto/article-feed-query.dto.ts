c2cc00560fe39182261a5a3831ffe7e691cb19dd
Yam Borodetsky <yam@yam.codes>
2026-01-28 00:42:22 +0500

    feat: Make article feed query limit and offset optional, parse string numeric values, and remove default assignments from the DTO.

--- src/articles/dto/article-feed-query.dto.ts
+++ src/articles/dto/article-feed-query.dto.ts
@@ -1,15 +1,9 @@
 import { type } from "arktype";
-import {
-	DEFAULT_LIMIT,
-	DEFAULT_OFFSET,
-	MAX_LIMIT,
-	MIN_LIMIT,
-	MIN_OFFSET,
-} from "@/shared/constants";
+import { MAX_LIMIT, MIN_LIMIT, MIN_OFFSET } from "@/shared/constants";
 
 export const ArticleFeedQueryDto = type({
-	limit: `${MIN_LIMIT} <= number.integer <= ${MAX_LIMIT} = ${DEFAULT_LIMIT}`,
-	offset: `number.integer >= ${MIN_OFFSET} = ${DEFAULT_OFFSET}`,
+	"limit?": `string.numeric.parse |> ${MIN_LIMIT} <= number.integer <= ${MAX_LIMIT}`,
+	"offset?": `string.numeric.parse |> number.integer >= ${MIN_OFFSET}`,
 });
 
 export type ArticleFeedQueryDto = typeof ArticleFeedQueryDto.infer;
